fa5e213e099ff394352a04ef78feba4e9d8d216d
cristian <segoviacristian23@hotmail.com>
2020-05-23 00:28:23 -0400

    chapter 8 mocking done

---
+++ 8.Mocking/countdown.go
@@ -0,0 +1,67 @@
+package Mocking
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+type CountdownOperationsSpy struct {
+	Calls []string
+}
+
+func (s *CountdownOperationsSpy) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	const finalWord = "Go!"
+	const countDownStart = 3
+	for i := countDownStart; i > 0; i-- {
+		sleeper.Sleep()
+		fmt.Fprintln(out, i)
+	}
+	sleeper.Sleep()
+	fmt.Fprintf(out, finalWord)
+}
+
+func main() {
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
+	Countdown(os.Stdout, sleeper)
+}
