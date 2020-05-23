32928183226c9b8b61e28d70e6a1feda4a7ea13c
cristian <segoviacristian23@hotmail.com>
2020-05-23 19:43:10 -0400

    9.concurrency

---
+++ 9.Concurrency/concurrency.go
@@ -0,0 +1,25 @@
+package Concurrency
+
+type WebsiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
+
+func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+	results := make(map[string]bool)
+	resultChannel := make(chan result)
+
+	for x, url := range urls {
+		go func(u string, i int) {
+			resultChannel <- result{u, wc(u)}
+		}(url, x)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		result := <-resultChannel
+		results[result.string] = result.bool
+	}
+
+	return results
+}
